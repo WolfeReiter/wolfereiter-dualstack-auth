427a38411bb2fde0b2ffe502fa99837a6d314340
Brian Reiter <breiter@wolfereiter.com>
2020-10-15 17:03:20 +0200

    Configure validation styling.

--- src/WolfeReiter.Identity.DualStack/wwwroot/js/unobtrusive.validation.bootstrap.js
+++ src/WolfeReiter.Identity.DualStack/wwwroot/js/unobtrusive.validation.bootstrap.js
@@ -1,41 +1,13 @@
 import "../lib/jquery/jquery.min.js";
+import "../lib/jquery-validation/jquery.validate.min.js"
+import "../lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js"
 
 "use strict"
-(function () {
-    if ($.validator && $.validator.unobtrusive) {
-        var defaultOptions = {
-            validClass: 'is-valid',
-            errorClass: 'is-invalid',
-            highlight: function (element, errorClass, validClass) {
-                $(element)
-                    .removeClass(validClass)
-                    .addClass(errorClass);
-            },
-            unhighlight: function (element, errorClass, validClass) {
-                $(element)
-                    .removeClass(errorClass)
-                    .addClass(validClass);
-            }
-        };
 
-        $.validator.setDefaults(defaultOptions);
+var settings = {
+    validClass: "is-valid",
+    errorClass: "is-invalid"
 
-        $.validator.unobtrusive.options = {
-            errorClass: defaultOptions.errorClass,
-            validClass: defaultOptions.validClass,
-            errorElement: 'div',
-            errorPlacement: function (error, element) {
-                error.addClass('invalid-feedback');
-
-                if (element.next().is(".input-group-append")) {
-                    error.insertAfter(element.next());
-                } else {
-                    error.insertAfter(element);
-                }
-            }
-        };
-    }
-    else {
-        console.warn('$.validator is not defined. Please load this library **after** loading jquery.validate.js and jquery.validate.unobtrusive.js');
-    }
-})();
+};
+$.validator.setDefaults(settings);
+$.validator.unobtrusive.options = settings;
