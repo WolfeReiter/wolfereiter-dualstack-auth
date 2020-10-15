33089c9be7b9a72652ac375368526c53f901f530
Brian Reiter <breiter@wolfereiter.com>
2020-10-15 15:29:35 +0200

    Name not nullable.

--- src/WolfeReiter.Identity.Data/Models/Role.cs
+++ src/WolfeReiter.Identity.Data/Models/Role.cs
@@ -11,6 +11,7 @@ namespace WolfeReiter.Identity.Data.Models
         {
             RoleId = Guid.NewGuid();
             UserRoles = new List<UserRole>();
+            Name = "";
         }
         
         [Key]
@@ -19,7 +20,7 @@ namespace WolfeReiter.Identity.Data.Models
 
         [Required]
         [MaxLength(250)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         public IList<UserRole> UserRoles { get; set; }
     }
